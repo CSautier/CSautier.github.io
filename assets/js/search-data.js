bfe849e2532ef40553a7e7d8ad102f26cc4139f7
CSautier <corentin.sautier@gmail.com>
2025-08-19 16:20:30 +0000

    Deploying to gh-pages from @ CSautier/CSautier.github.io@05212f47c3053367c6d88a6a1b898e18a092e4a8 🚀

---
+++ assets/js/search-data.js
@@ -0,0 +1,101 @@
+// get the ninja-keys element
+const ninja = document.querySelector('ninja-keys');
+
+// add the home and posts menu items
+ninja.data = [{
+    id: "nav-about",
+    title: "About",
+    section: "Navigation",
+    handler: () => {
+      window.location.href = "/";
+    },
+  },{id: "nav-publications",
+          title: "Publications",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/publications/";
+          },
+        },{id: "nav-repositories",
+          title: "Repositories",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/repositories/";
+          },
+        },{id: "nav-unit-unsupervised-online-instance-segmentation-through-time",
+          title: "UNIT: Unsupervised Online Instance Segmentation through Time",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/unit/";
+          },
+        },{
+        id: 'social-dblp',
+        title: 'DBLP',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://dblp.org/pid/317/5523.html", "_blank");
+        },
+      },{
+        id: 'social-email',
+        title: 'email',
+        section: 'Socials',
+        handler: () => {
+          window.open("mailto:%63%6F%72%65%6E%74%69%6E.%73%61%75%74%69%65%72@%65%6E%70%63.%66%72", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/CSautier", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/corentin-sautier-74415917b", "_blank");
+        },
+      },{
+        id: 'social-scholar',
+        title: 'Google Scholar',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://scholar.google.com/citations?user=xYDkHEsAAAAJ", "_blank");
+        },
+      },{
+        id: 'social-semanticscholar',
+        title: 'Semantic Scholar',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.semanticscholar.org/author/Corentin-Sautier/2160711622# your Semantic Scholar ID", "_blank");
+        },
+      },{
+      id: 'light-theme',
+      title: 'Change theme to light',
+      description: 'Change the theme of the site to Light',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("light");
+      },
+    },
+    {
+      id: 'dark-theme',
+      title: 'Change theme to dark',
+      description: 'Change the theme of the site to Dark',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("dark");
+      },
+    },
+    {
+      id: 'system-theme',
+      title: 'Use system default theme',
+      description: 'Change the theme of the site to System Default',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("system");
+      },
+    },];
--- assets/potree/libs/three.js/loaders/obj2/worker/parallel/OBJLoader2JsmWorker.js
+++ assets/potree/libs/three.js/loaders/obj2/worker/parallel/OBJLoader2JsmWorker.js
@@ -1,12 +1,1 @@
-/**
- * Development repository: https://github.com/kaisalmen/WWOBJLoader
- */
-
-import { OBJLoader2Parser } from '../../OBJLoader2Parser.js';
-
-import {
-	WorkerRunner,
-	DefaultWorkerPayloadHandler
-} from './WorkerRunner.js';
-
-new WorkerRunner( new DefaultWorkerPayloadHandler( new OBJLoader2Parser() ) );
+import{OBJLoader2Parser}from"../../OBJLoader2Parser.js";import{WorkerRunner,DefaultWorkerPayloadHandler}from"./WorkerRunner.js";new WorkerRunner(new DefaultWorkerPayloadHandler(new OBJLoader2Parser));
